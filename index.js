9bafba7b8da9c48fda956ebbe582dac78ae07100
Mohsin raza <mohsin552001@gmail.com>
2022-08-04 19:01:42 +0500

    at this postion i have created like that bottom header

---
+++ index.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.querySelector(".companyproducts .productfeatures");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showsignupbtns() {
+    var x = document.querySelector(".companyproducts .signinupbtns");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
