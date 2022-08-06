5ea5ebc205884f801514c7fdf2e994de4583b5ff
Mohsin raza <mohsin552001@gmail.com>
2022-08-06 12:47:11 +0500

    at this position i have almost formed this site completely with responsive

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function myFunction() {
+    let y = document.querySelector('.cloud .infrastructure .inputsection')
     var x = document.querySelector(".companyproducts .productfeatures");
-    if (x.style.display === "none") {
+    if (x.style.display === "none", y.style.display === 'block') {
         x.style.display = "block";
+        y.style.display = "none"
     } else {
         x.style.display = "none";
+        y.style.display = "block"
     }
 }
 
 function showsignupbtns() {
+
     var x = document.querySelector(".companyproducts .signinupbtns");
     if (x.style.display === "none") {
         x.style.display = "block";
+
     } else {
         x.style.display = "none";
+
     }
 }
